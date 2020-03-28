d4c2b54b8847ac51deabb70fa8a29ca9e3df19b9
Mark Palfreeman <mapalfre@microsoft.com>
2020-03-27 22:04:41 -0700

    trim some excess

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,26 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
-import styled from 'styled-components'
-import { layout, screenSize } from '../styles/variables'
+import Layout from "../components/Layout";
+import styled from "styled-components";
+import { layout, screenSize } from "../styles/variables";
 
 function Index(props) {
   return (
     <Layout>
       <div className="container">
         <h2>I'm Mark.</h2>
-        <p>
-          I'm a UI engineer in the Seattle area building delightful modern user
-          experiences on the web.
-        </p>
-        <p>
-          I'm also an advocate for people-first, collaborative company culture,
-          remote work, and constant learning.
-        </p>
+        <p>I'm a UI engineer in the Seattle area building modern, maintainable user experiences on the web.</p>
+        <p>I'm also an advocate for people-first company culture, remote work, and constant learning.</p>
         <hr />
         <p>
-          This site is a constant work in progress as I learn new things, and
-          the code is public on my{' '}
+          This site is a constant work in progress, and the code is public on my{" "}
           <a href="https://github.com/markpalfreeman">GitHub</a>.
         </p>
         <p>
-          Feel free to reach me on Twitter at{' '}
-          <a href="https://twitter.com/markpalfreeman">@markpalfreeman</a> or by
+          Feel free to reach me on Twitter at <a href="https://twitter.com/markpalfreeman">@markpalfreeman</a> or by
           email at <a href="mailto:mark@palf.co">mark@palf.co</a>.
         </p>
       </div>
     </Layout>
-  )
+  );
 }
 
-export default Index
+export default Index;
