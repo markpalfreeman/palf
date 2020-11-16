60ba4d03e2fc0896ac8388e313e9260da1489403
Mark <Mark@palf.lan>
2020-11-16 07:05:03 -0800

    edit home section of /now page

--- pages/now.js
+++ pages/now.js
@@ -5,27 +5,28 @@ function Now() {
     <Layout>
       <div className="container">
         <h1>What I’m Doing Now</h1>
-        <h2>🏠 Staying at Home</h2>
+        <h2>🏠 We moved!</h2>
         <p>
-          I live right in the little town of Snohomish, WA about 25 miles northeast of Seattle. Our family loves walking
-          around town—within minutes we can visit shopping, coffee, wineries, parks, or the library. With the
-          coronavirus, I'm trying to stay productive in close quarters with a 3- and 1-year old. And maybe get outside
-          for some exercise to stay sane and mix up the week.
+          After almost 4 years in the small town of Snohomish, we moved our family into a bigger
+          house in Lynnwood, halfway closer to Seattle. We've been acclimating the kids, connecting
+          with friends, painting, doing large exterior projects, and are now replacing carpet floor.
         </p>
         <p>
-          We’re hoping to move south soon, so now it’s go-time searching for a new house in the strangest real estate
-          market since the last recession. I’ve rebuilt our front picket fence and plan to declutter (and wait out
-          government bans) until we can get our house listed for sale.
+          And yet, we'll be home-bound again while the state of Washington attempts to slow the
+          spread of COVID-19. It's frustrating that the holidays are going to look different this
+          year with all the government restrictions on gatherings, travel, and entertainment, but it
+          should yield more family connection and time for home improvement.
         </p>
         <h2>📖 Reading</h2>
         <p>
-          I love reading and connecting over books. I’ve been meeting monthly with a small book club (me and two
-          friends) to try to finish more books.
+          I love reading and connecting over books. I’ve been meeting monthly with a small book club
+          (me and two friends) to try to finish more books.
         </p>
         <p>I’m currently reading:</p>
         <ul>
           <li>
-            <a href="https://www.goodreads.com/book/show/40121378-atomic-habits">Atomic Habits</a> by James Clear
+            <a href="https://www.goodreads.com/book/show/40121378-atomic-habits">Atomic Habits</a>{" "}
+            by James Clear
           </li>
           <li>
             <a href="https://www.goodreads.com/book/show/43982455-the-ruthless-elimination-of-hurry">
@@ -36,14 +37,15 @@ function Now() {
         </ul>
         <h2>💻 UI Development </h2>
         <p>
-          I’m at Microsoft working on a UI library of controls and components for use on the home and store pages of{" "}
-          <a href="https://microsoft.com">microsoft.com</a>. It’s been a lesson “back to the basics” writing plain old
-          JavaScript, HTML, and CSS, along with a Handlebars documentation site. I’d like to return to React and more
-          app-level development again, but spending time with libraries at the lowest level forces me to go deeper on
-          the essentials, accessibility, versioning, and other critical considerations of web development.
+          I’m at Microsoft working on a UI library of controls and components for use on the home
+          and store pages of <a href="https://microsoft.com">microsoft.com</a>. It’s been a lesson
+          “back to the basics” writing plain old JavaScript, HTML, and CSS, along with a Handlebars
+          documentation site. I’d like to return to React and more app-level development again, but
+          spending time with libraries at the lowest level forces me to go deeper on the essentials,
+          accessibility, versioning, and other critical considerations of web development.
         </p>
         <small>
-          <em>Updated: March 28, 2020</em>
+          <em>Updated: November 16, 2020</em>
         </small>
       </div>
     </Layout>
