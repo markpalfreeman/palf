87930db741812de8be50cc55c6946d739d25f1cd
Mark Palfreeman <mapalfre@microsoft.com>
2023-01-01 06:20:50 -0800

    format files with prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'http://palf.co',
-  integrations: [mdx(), sitemap()]
+  site: "http://palf.co",
+  integrations: [mdx(), sitemap()],
 });
