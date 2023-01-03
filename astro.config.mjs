e164ed73fbb28ac236a3d3e24ec672ac7b7492dc
Mark Palfreeman <mapalfre@microsoft.com>
2023-01-03 08:42:28 -0800

    wip: images, work, 404, fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
+import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://palf.io",
-  integrations: [mdx(), sitemap()],
+  integrations: [image(), mdx(), sitemap()],
 });
